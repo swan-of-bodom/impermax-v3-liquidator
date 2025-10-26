b8a770367eae30a92cc75c43ce4d215a855e76ef
swan-of-bodom <matias@impermax.finance>
2025-10-26 21:14:40 +0100

    tests

--- test/ImpermaxV3Liquidator.t.sol
+++ test/ImpermaxV3Liquidator.t.sol
@@ -9,14 +9,16 @@ contract ImpermaxV3LiquidatorTest is Test {
 
     // Impermax Router on this chain
     address constant ROUTER = 0x0fD27DC61e2Dc85EF63298E39cB2879432F2DaF6;
+    // Extension router on this chain (Uniswap's SwapRouter for UniV3Liquidator, etc.)
+    address uniswapRouter = 0x2626664c2603336E57B271c5C0b26F421741e481;
 
     // Position to liquidate
     address constant NFTLP = 0x62Eb5c0f829e7fAE4da4B1bDE3b1540F581Fc187;
     uint256 constant TOKEN_ID = 39;
 
     function setUp() public {
-        vm.createSelectFork("hyperevm");
-        liquidator = new UniV3Liquidator(ROUTER);
+        vm.createSelectFork("base");
+        liquidator = new UniV3Liquidator(ROUTER, uniswapRouter);
     }
 
     function test_flashLiquidate() public {
